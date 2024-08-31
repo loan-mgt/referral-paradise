d18358468cb45b0b05d092b580bc55e6d3e6cb45
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2024-08-31 13:08:31 +0000

    fix bug docker

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,65 +1,67 @@
 package db
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "os"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/joho/godotenv"
-    "github.com/golang-migrate/migrate/v4"
-    "github.com/golang-migrate/migrate/v4/database/mysql"
-    _ "github.com/golang-migrate/migrate/v4/source/file" // Ensure file source is available for migrations
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file" // Ensure file source is available for migrations
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func Connect() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 
-    DB, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_PORT"),
-        os.Getenv("DB_NAME"),
-    ))
-    if err != nil {
-        log.Fatal(err)
-    }
+	log.Println(os.Getenv("DB_HOST"))
+
+	DB, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Close() {
-    DB.Close()
+	DB.Close()
 }
 
 func RunMigrations() {
-    driver, err := mysql.WithInstance(DB, &mysql.Config{})
-    if err != nil {
-        log.Fatal(err)
-    }
+	driver, err := mysql.WithInstance(DB, &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    m, err := migrate.NewWithDatabaseInstance(
-        "file://migrations",  // Path to your migrations folder
-        os.Getenv("DB_NAME"), driver)
-    if err != nil {
-        log.Fatal(err)
-    }
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations", // Path to your migrations folder
+		os.Getenv("DB_NAME"), driver)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-        log.Fatal(err)
-    }
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
 }
 
 func GetTableSize() (int, error) {
-    var count int
-    err := DB.QueryRow("SELECT COUNT(*) FROM ref_code").Scan(&count)
-    if err != nil {
-        return 0, err
-    }
-    return count, nil
+	var count int
+	err := DB.QueryRow("SELECT COUNT(*) FROM ref_code").Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
 }
